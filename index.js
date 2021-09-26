524ed71572e7def7af221dee6f141855928e10a6
KyalSinLinLett <alexsin0012@gmail.com>
2021-09-26 20:47:50 +0630

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require("http")
+const fs = require("fs")
+
+const httpServer = http.createServer()
+
+httpServer.on('listening', () => console.log("Listening..."))
+
+httpServer.on('request', (req, res) => {
+
+    if (req.url === "/") {
+        res.end(fs.readFileSync("index.html"))
+        return;
+    }
+
+    if (req.url === "/upload") {
+        const fileName = req.headers['file-name']
+        req.on("data", chunk => {
+            console.log("RECEIVED CHUNK" + `${chunk.length}`)
+            fs.appendFileSync(fileName, chunk)
+        })
+
+        res.end("uploaded!")
+    }
+
+})
+
+httpServer.listen(8080)
